fcea5d7d22b26f6a53199fed39c449bf57bd4118
MateuszBogacki <manti.2@vp.pl>
2016-11-30 12:47:36 +0100

    Commit

--- SnakeMB/Form1.cs
+++ SnakeMB/Form1.cs
@@ -13,40 +13,56 @@ namespace SnakeMB
     public partial class Form1 : Form
     {
         int i;
-        
+        int kierunek = 1; // 1 = prawo, 2 = lewo, 3 = góra, 4 = dół
         List<Snake> snake = new List<Snake>(); 
        
         Timer gameLoop = new Timer();
+        Timer snakeLoop = new Timer();
 
         public Form1()
         {
             InitializeComponent();
             snake.Clear();
-            gameLoop.Interval = 1000 / 60;
+            gameLoop.Tick += new EventHandler(kierunekRuchu);
+            snakeLoop.Tick += new EventHandler(moveSnake);
             gameLoop.Start();
-            playground.Invalidate();
-            Snake head = new Snake(10,10);
+            snakeLoop.Start();
+            Snake head = new Snake(0,0);
             snake.Add(head);
-
+        
 
         }
 
+        private void kierunekRuchu(object sender, EventArgs e)
+        {
+            if (Press.Button(Keys.Left))
+            {
+                kierunek = 1;
+            }
+            playground.Invalidate();
+        } 
+
+        private void moveSnake(object sender, EventArgs e)
+        {
+            for (int j = snake.Count -1 ; j >= 0; j--)
+            {
+               
+                if (kierunek == 1) snake[0].X++;
+
+            }
+        }
 
         private void playground_Paint(object sender, PaintEventArgs e)
         {
             Draw(e.Graphics);
         }
-
-       
-
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-
+            Press.wcisnijPusc(e.KeyCode, true);
         }
-
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-
+            Press.wcisnijPusc(e.KeyCode, false);
         }
 
         private void Draw(Graphics graphics)
@@ -55,5 +71,6 @@ namespace SnakeMB
             Snake actualPart = snake[i];
             graphics.FillRectangle(new SolidBrush(kolor_weza), new Rectangle(actualPart.X * 16, actualPart.Y * 16, 16, 16));
         }
+       
     }
 }
---
+++ SnakeMB/Press.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeMB
+{
+
+    public static class Press
+    {
+        public static Hashtable _keys = new Hashtable();
+
+        public static void wcisnijPusc(Keys key, bool i)
+        {
+            _keys[key] = i;
+        }
+
+        public static bool Button(Keys key)
+        {
+            if(_keys[key] == null)
+             _keys[key] = false;
+                return (bool)_keys[key];
+            
+        }
+    }
+}
